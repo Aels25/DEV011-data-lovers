c3bb29096a62407edf4bcfa1c4a6b3c25c5fe3d8
Aels25 <alessandraemp13@gmail.com>
2023-09-27 15:28:23 -0500

    Se muestra las cartas de los pokemones con algunos datos. En cada carta un botón que agrega más información. Falta agregar los botones de filtrado y ordenado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,85 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
-
-// import data from './data/lol/lol.js';
+// Importa los datos de Pokémon desde el archivo correspondiente (data/pokemon/pokemon.js).
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, renderItems, data);
+document.addEventListener('DOMContentLoaded', () => {
+    const openPokemonButton = document.getElementById('openPokemonButton');
+    const cardsContainer = document.querySelector('.pokemon-cards-container');
+
+    openPokemonButton.addEventListener('click', () => {
+        // Oculta los elementos que deseas ocultar
+        document.querySelector('.buttons.left').style.display = 'none';
+        document.querySelector('.buttons.right').style.display = 'none';
+        document.getElementById('welcomeHeader').style.display = 'none';
+        document.querySelector('.pokemon-image').style.display = 'none';
+        document.getElementById('openPokemonButton').style.display = 'none';
+        document.getElementById('discoverHeading').style.display = 'none';
+        document.getElementById('footer').style.display = 'none';
+
+
+        
+
+    
+        // Muestra el contenedor de cartas
+        cardsContainer.style.display = 'flex';
+    
+        // Muestra las cartas de Pokémon en 4 columnas
+        renderPokemonCards(data.pokemon, cardsContainer, 4);
+    });
+    
+    
+    
+    
+    
+});
+
+// Agrega una función para renderizar las cartas de Pokémon en una sola columna
+function renderPokemonCards(pokemonList, container) {
+    container.innerHTML = ''; // Limpia el contenido anterior
+
+    pokemonList.forEach((pokemon) => {
+        renderPokemonCard(pokemon, container);
+    });
+}
+
+// Agrega una función para renderizar una carta de Pokémon
+function renderPokemonCard(pokemon, container) {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+
+    // Agrega aquí el contenido de la carta de Pokémon, incluido el botón de "Ver Descripción"
+    card.innerHTML = `
+        <h3>Número de la Pokédex: ${pokemon.num}</h3>
+        <img src="${pokemon.img}" alt="${pokemon.name}">
+        <h3>Nombre: ${pokemon.name}</h3>
+        <button class="description-button">Ver Descripción</button>
+        <div class="description" style="display: none;">
+            <p>Generación: ${pokemon.generation.name}</p>
+            <p>Descripción: ${pokemon.about}</p>
+            <p>Tamaño: ${pokemon.size.height} / ${pokemon.size.weight}</p>
+            <p>Rareza: ${pokemon['pokemon-rarity']}</p>
+            <p>Tipo: ${pokemon.type.join(', ')}</p>
+            <p>Probabilidad de Aparición: ${pokemon['spawn-chance']}</p>
+            <p>Resistencias: ${pokemon.resistant.join(', ')}</p>
+            <p>Debilidades: ${pokemon.weaknesses.join(', ')}</p>
+            <p>Movimientos Rápidos: ${pokemon['quick-move'].map(move => move.name).join(', ')}</p>
+            <p>Ataques Especiales: ${pokemon['special-attack'].map(attack => attack.name).join(', ')}</p>
+            <p>Huevo: ${pokemon.egg}</p>
+            <p>Distancia de Compañero: ${pokemon['buddy-distance-km']} km</p>
+            <p>Evolución: ${pokemon.evolution ? `Candy: ${pokemon.evolution.candy}` : 'No evoluciona'}</p>
+        </div>
+    `;
+
+    container.appendChild(card);
+
+    // Agrega un evento de clic al botón de "Ver Descripción" para mostrar/ocultar la descripción
+    const descriptionButton = card.querySelector('.description-button');
+    const description = card.querySelector('.description');
+
+    descriptionButton.addEventListener('click', () => {
+        if (description.style.display === 'none' || description.style.display === '') {
+            description.style.display = 'block'; // Muestra la descripción
+        } else {
+            description.style.display = 'none'; // Oculta la descripción
+        }
+    });
+}
--- src/view.js
+++ src/view.js
@@ -1,6 +1,28 @@
-// estas funciones son de ejemplo
+// view.js
 
-export const renderItems = () => {
-  return 'example';
+export const renderItems = (pokemonList, rootElement) => {
+  console.log("Renderización iniciada");
+
+  const container = document.createElement('div');
+  container.classList.add('pokemon-container');
+
+  pokemonList.forEach(pokemon => {
+      const card = document.createElement('div');
+      card.classList.add('pokemon-card');
+
+      card.innerHTML = `
+          <h3>Número de la Pokédex: ${pokemon.num}</h3>
+          <h3>Nombre: ${pokemon.name}</h3>
+          <p>Generación: ${pokemon.generation.name}</p>
+          <p>Descripción: ${pokemon.about}</p>
+          <!-- Agrega el resto de los datos del Pokémon aquí -->
+      `;
+
+      container.appendChild(card);
+  });
+
+  rootElement.innerHTML = ''; // Limpia el contenido anterior en el elemento root
+  rootElement.appendChild(container);
+
+  console.log("Renderización completada");
 };
-
